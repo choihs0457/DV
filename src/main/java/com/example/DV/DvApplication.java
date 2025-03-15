28b94d6abaf8c64367f14c1392226a04a8308e68
unknown <choihs0457@gmail.com>
2025-03-16 00:18:53 +0900

    init

---
+++ src/main/java/com/example/DV/DvApplication.java
@@ -0,0 +1,13 @@
+package com.example.DV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvApplication.class, args);
+	}
+
+}
